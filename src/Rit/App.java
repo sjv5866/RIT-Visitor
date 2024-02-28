5cf6629e245973a878a9af1bd10f0616db6268a4
Samuel Velasquez <samuelvelasquez@protonmail.com>
2024-02-27 20:40:16 -0500

    just push into main ftlog

--- src/Rit/App.java
+++ src/Rit/App.java
@@ -2,7 +2,7 @@ package Rit;
 
 public class App {
     public static void main(String[] args) {
-        System.out.print("hello world ");
+        System.out.print("hello wo");
 
     }
 }
---
+++ src/Rit/RITVisitor.java
@@ -0,0 +1,4 @@
+package Rit;
+
+public interface RITVisitor {
+}
